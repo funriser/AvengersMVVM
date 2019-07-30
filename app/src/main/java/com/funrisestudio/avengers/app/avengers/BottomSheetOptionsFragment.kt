d486380a85fc456f47705e6a12808290b30523f3
funriser <kost.maksym@gmail.com>
2019-07-30 18:29:36 +0300

    Bottom sheet expanding

--- app/src/main/java/com/funrisestudio/avengers/app/avengers/BottomSheetOptionsFragment.kt
+++ app/src/main/java/com/funrisestudio/avengers/app/avengers/BottomSheetOptionsFragment.kt
@@ -4,13 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.doOnLayout
 import com.funrisestudio.avengers.R
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import kotlinx.android.synthetic.main.fragment_bottom_sheet_options.*
 
 class BottomSheetOptionsFragment : BottomSheetDialogFragment() {
-
-    private lateinit var behavior: BottomSheetBehavior<View>
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_bottom_sheet_options, container, false)
@@ -18,7 +18,11 @@ class BottomSheetOptionsFragment : BottomSheetDialogFragment() {
 
     override fun onResume() {
         super.onResume()
-        behavior = BottomSheetBehavior.from(view?.parent as View)
+        view?.doOnLayout {
+            BottomSheetBehavior.from(it.parent as View).apply {
+                setPeekHeight(vHidden.top)
+            }
+        }
     }
 
 }
