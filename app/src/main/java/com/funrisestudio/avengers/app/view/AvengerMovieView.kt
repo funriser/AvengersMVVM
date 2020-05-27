058d513e9e76856f64d38ee8ae759d4711bceb39
funriser <kost.maksym@gmail.com>
2020-05-27 22:46:46 +0300

    Target sdk updated to 29. Parcelable entities fixed.

--- app/src/main/java/com/funrisestudio/avengers/app/view/AvengerMovieView.kt
+++ app/src/main/java/com/funrisestudio/avengers/app/view/AvengerMovieView.kt
@@ -1,33 +1,10 @@
 package com.funrisestudio.avengers.app.view
 
-import android.os.Parcel
 import android.os.Parcelable
 import com.funrisestudio.avengers.domain.entity.AvengerMovie
+import kotlinx.android.parcel.Parcelize
 
-class AvengerMovieView(val name: String, val poster: String) : Parcelable {
-
+@Parcelize
+data class AvengerMovieView(val name: String, val poster: String) : Parcelable {
     constructor (avengerMovie: AvengerMovie) : this(avengerMovie.name, avengerMovie.poster)
-
-    constructor(parcel: Parcel) : this(
-            parcel.readString(),
-            parcel.readString())
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(name)
-        parcel.writeString(poster)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<AvengerMovieView> {
-        override fun createFromParcel(parcel: Parcel): AvengerMovieView {
-            return AvengerMovieView(parcel)
-        }
-
-        override fun newArray(size: Int): Array<AvengerMovieView?> {
-            return arrayOfNulls(size)
-        }
-    }
 }
